4f7bb8caeccd5e7526ad2894b84b5e0a323f291b
chelm <christopher.helm@gmail.com>
2015-05-28 11:52:28 -0600

    pulling d3 queue down

--- examples/dojo-config.js
+++ examples/dojo-config.js
@@ -1,6 +1,5 @@
 var dojoConfig = { 
   paths: {
-    plugins: location.pathname.replace(/\/[^/]+$/, "") + "/../src",
-    queue: "http://d3js.org/queue.v1.min"
+    plugins: location.pathname.replace(/\/[^/]+$/, "") + "/../src"
   }
 };
