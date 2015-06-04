c4a29d1ba84ebb81d0e294fbdf38f54086547e37
Sam Libby <slibby@esri.com>
2015-06-04 09:16:52 -0400

    remove explicit-http from dojo-config

--- examples/dojo-config.js
+++ examples/dojo-config.js
@@ -1,6 +1,6 @@
 var dojoConfig = { 
   paths: {
     plugins: location.pathname.replace(/\/[^/]+$/, "") + "/../src",
-    queue: "http://d3js.org/queue.v1.min"
+    queue: "//d3js.org/queue.v1.min"
   }
 };
